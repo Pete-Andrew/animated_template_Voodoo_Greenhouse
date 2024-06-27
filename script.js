94728ca49e428230fd367575b2ca75e9a80451bf
Peter Andrew <peter.a.andrew@gmail.com>
2024-06-27 11:29:25 +0100

    created repo

---
+++ script.js
@@ -0,0 +1,8 @@
+AOS.init({
+    // sets a minimum for everything
+    delay: 200, //values from 0 - 3000, with step 50ms
+    duration: 800, //values from 0 - 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, //whether animation should happen only once whilst scrolling down
+    mirror: false, // whether elements should animate out whilst scrolling past
+  });
