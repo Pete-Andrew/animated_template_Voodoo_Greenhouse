bbbe4ad2eb5a64d647b707048347acdb69971462
Peter Andrew <peter.a.andrew@gmail.com>
2024-07-15 13:22:35 +0100

    fixed the side nav so it does not appear on load

--- navbar.js
+++ navbar.js
@@ -1,4 +1,13 @@
-function toggleNavbar(collapseID) {
-    document.getElementById(collapseID).classList.toggle("hidden");
-    document.getElementById(collapseID).classList.toggle("block");
-  }
+const openNavButton = document.getElementById('openNavButton');
+const closeNavButton = document.getElementById('closeNavButton');
+
+// code for the side nav bar 
+ openNavButton.addEventListener('click', function () {
+  document.getElementById("mySidenav").style.width = "350px";
+  console.log("open nav bar clicked");
+
+});
+
+closeNavButton.addEventListener('click', function () {
+  document.getElementById("mySidenav").style.width = "0px";
+});
